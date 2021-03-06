7f48e88678ede8f7a9c5053a550aed3724f7e5b3
sllchoo2095 <sllchoo8211@gmail.com>
2021-03-06 16:23:42 +1100

    realtime db set up student class

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,5 +1,8 @@
 from flask import Flask
 from flask_restful import Resource, Api
+from firebase_util import firebase
+from datetime import datetime, date
+import json
 
 app = Flask(__name__)
 api = Api(app)
@@ -10,5 +13,16 @@ class HelloWorld(Resource):
 
 api.add_resource(HelloWorld, '/')
 
+
 if __name__ == '__main__':
+    db = firebase.database()
+    d = datetime.strptime("20.12.2010 09:38:42,76", "%d.%m.%Y %H:%M:%S,%f").strftime('%s.%f')
+    data = {"firstName": "Mort",
+        "lastName": "Smith", 
+        "birthday": int(float(d)*1000),
+        "email": "mortSmith@gmail.com", 
+        "gender": "Male", 
+        "location": "Carlton", 
+        "disadvantaged": False}
+    db.child("users").push(data)
     app.run(debug=True)
